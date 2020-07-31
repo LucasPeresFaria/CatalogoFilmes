2533e707eb87a42e22609635bbfd2e07faedb4ef
LucasPeresFaria <48368808+LucasPeresFaria@users.noreply.github.com>
2020-07-30 21:31:13 -0300

    pesquisa infofilme

--- View/js/script.js
+++ View/js/script.js
@@ -11,31 +11,31 @@ function loadFilms() {
       saida += '<div class="container">';
         saida += '<div class="row ml-5">';
 
-          saida += '<div class="col-4">';
-          saida += '<img src="' + films[i].img + '" width="290" height="490">';
-          saida += '</div>';
+            saida += '<div class="col-4">';
+              saida += '<img src="' + films[i].img + '" width="290" height="490">';
+            saida += '</div>';
 
-          saida += '<div class="col-5">';
-          saida += '<h4>"' + films[i].titulo + '"</h4>';
-          saida += '<p>"' + films[i].descricao + '"</p>';
+            saida += '<div class="col-5">';
+              saida += '<h4>"' + films[i].titulo + '"</h4>';
+              saida += '<p>"' + films[i].descricao + '"</p>';
 
-          saida += ' <div class="row">';
-          saida += '<img class="mt-1 ml-2 mr-5" src="https://image.flaticon.com/icons/svg/1146/1146634.svg" width="50" height="50">';
-          saida += '<p class="mt-3 ml-5"><strong>Direção: </strong>' + films[i].direcao + '"</p>';
-          saida += '</div>';
+                saida += ' <div class="row">';
+                  saida += '<img class="mt-1 ml-2 mr-5" src="https://image.flaticon.com/icons/svg/1146/1146634.svg" width="50" height="50">';
+                  saida += '<p class="mt-3 ml-5"><strong>Direção: </strong>' + films[i].direcao + '"</p>';
+                saida += '</div>';
 
-          saida += '<p class="mt-1">"' + films[i].critica + '"</p>';
-          saida += '</div>';  //col-lg-5
+                saida += '<p class="mt-1">"' + films[i].critica + '"</p>';
+              saida += '</div>';  //col-5
 
-          saida += '</div>'; //row
+        saida += '</div>'; //row ml-5
 
           saida += ' <div class="row ml-5">';
-          saida += '<img class="ml-3 mt-1" src="https://cdn.icon-icons.com/icons2/37/PNG/512/clock_theapplication_2900.png" width="25" height="25">';
-          saida += '<p class="mt-1 ml-2">"' + films[i].duracao + '" | </p>';
-          saida += '<p class="mt-1 ml-2">" Lançamento: ' + films[i].lancamento + '"</p>';
-        saida += '</div>';
-      saida += '</div>';
+            saida += '<img class="ml-3 mt-1" src="https://cdn.icon-icons.com/icons2/37/PNG/512/clock_theapplication_2900.png" width="25" height="25">';
+            saida += '<p class="mt-1 ml-2">"' + films[i].duracao + '" | </p>';
+            saida += '<p class="mt-1 ml-2">" Lançamento: ' + films[i].lancamento + '"</p>';
+          saida += '</div>';
 
+      saida += '</div>';
 
     }
 
@@ -48,6 +48,7 @@ function Pesquisa() {
 
   let saida = '';
   let search = document.getElementById('txtFilme').value;
+  let flag = 0;
 
   $.getJSON("js/json/Catalogo.json", function (data) {
     films = data.films;
@@ -55,37 +56,54 @@ function Pesquisa() {
     for (i = 0; i < films.length; i++) {
 
       if (films[i].titulo == search){
+
+          flag = 1;
+
           saida += '<div class="container">';
-          saida += '<div class="row ml-5">';
+            saida += '<div class="row ml-5">';
 
-          saida += '<div class="col-4">';
-          saida += '<img src="' + films[i].img + '" width="290" height="490">';
-          saida += '</div>';
+                saida += '<div class="col-4">';
+                  saida += '<img src="' + films[i].img + '" width="290" height="490">';
+                saida += '</div>';
 
-          saida += '<div class="col-5">';
-          saida += '<h4>"' + films[i].titulo + '"</h4>';
-          saida += '<p>"' + films[i].descricao + '"</p>';
+                saida += '<div class="col-5">';
+                  saida += '<h4>"' + films[i].titulo + '"</h4>';
+                  saida += '<p>"' + films[i].descricao + '"</p>';
 
-          saida += ' <div class="row">';
-          saida += '<img class="mt-1 ml-2 mr-5" src="https://image.flaticon.com/icons/svg/1146/1146634.svg" width="50" height="50">';
-          saida += '<p class="mt-3 ml-5"><strong>Direção: </strong>' + films[i].direcao + '"</p>';
-          saida += '</div>';
+                  saida += ' <div class="row">';
+                    saida += '<img class="mt-1 ml-2 mr-5" src="https://image.flaticon.com/icons/svg/1146/1146634.svg" width="50" height="50">';
+                    saida += '<p class="mt-3 ml-5"><strong>Direção: </strong>' + films[i].direcao + '"</p>';
+                  saida += '</div>';
 
-          saida += '<p class="mt-1">"' + films[i].critica + '"</p>';
-          saida += '</div>';  //col-lg-5
+                  saida += '<p class="mt-1">"' + films[i].critica + '"</p>';
+                saida += '</div>';  //col-5
 
-          saida += '</div>'; //row
+            saida += '</div>'; //row
 
-          saida += ' <div class="row ml-5">';
-          saida += '<img class="ml-3 mt-1" src="https://cdn.icon-icons.com/icons2/37/PNG/512/clock_theapplication_2900.png" width="25" height="25">';
-          saida += '<p class="mt-1 ml-2">"' + films[i].duracao + '" | </p>';
-          saida += '<p class="mt-1 ml-2">" Lançamento: ' + films[i].lancamento + '"</p>';
-          saida += '</div>';
+            saida += ' <div class="row ml-5">';
+              saida += '<img class="ml-3 mt-1" src="https://cdn.icon-icons.com/icons2/37/PNG/512/clock_theapplication_2900.png" width="25" height="25">';
+              saida += '<p class="mt-1 ml-2">"' + films[i].duracao + '" | </p>';
+              saida += '<p class="mt-1 ml-2">" Lançamento: ' + films[i].lancamento + '"</p>';
+            saida += '</div>';
+
           saida += '</div>';
       }
 
         document.getElementById('tela').innerHTML = saida;
     }
-    
+
+    if(flag==0){
+      let notfound = '';
+        
+        notfound += '<div class="jumbotron jumbotron-fluid">';
+          notfound += '<div class="container">';
+            notfound += '<h1 class="display-4">Filme não encontrado :(</h1>';
+            notfound += '<p class="lead">Não foi possível encontrar o filme que está pesquisando...</p>';
+          notfound += '</div>';
+        notfound += '</div>';
+
+      document.getElementById('tela').innerHTML = notfound;
+    }
+
   });
 }
