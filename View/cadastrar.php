d7905aefa4af6e33359dc4de8698c5719182d4a7
LucasPeresFaria <48368808+LucasPeresFaria@users.noreply.github.com>
2020-07-30 22:37:37 -0300

    cadastro campos

--- View/cadastrar.php
+++ View/cadastrar.php
@@ -9,28 +9,62 @@
 <section>
 
   <form class="mb-5 mt-3" method="POST" action="">
+    <div class="ml-4 col-11">
 
-    <div class="form-group ml-5 col-6">
+      <div class="row ml-1">
+        <div class="col-6 mb-3">
 
-      <label for="txtNome">Título</label>
-      <input id="txtNome" type="text" class="form-control" aria-describedby="nome" placeholder="digite aqui" required name=txtNome />
+          <div class="mb-3">
+            <label for="txtNome">Título</label>
+            <input id="txtNome" type="text" class="form-control" aria-describedby="nome" placeholder="digite aqui" required name=txtNome />
+          </div>
 
-      <div class="input-group mt-2 ml-1 row">
-        <div class="input-group-prepend">
-          <span class="input-group-text">Descrição</span>
+          <div class="row">
+            <div class="mb-3 col-3">
+              <label for="txtNome">Duração</label>
+              <input id="txtNome" type="text" class="form-control" aria-describedby="nome" placeholder="digite aqui" required name=txtNome />
+            </div>
+            <div class="mb-3 col-9">
+              <label for="txtEmail">Lançamento</label>
+              <input id="txtEmail" type="email" class="form-control" aria-describedby="e-mail" name="txtEmail" placeholder="digite aqui" required name=txtEmail />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col mb-3">
+              <label for="txtEmail">Direção</label>
+              <input id="txtEmail" type="email" class="form-control" aria-describedby="e-mail" name="txtEmail" placeholder="digite aqui" required name=txtEmail />
+            </div>
+
+            <div class="col mb-3">
+              <label for="txtEmail">Crítica</label>
+              <input id="txtEmail" type="email" class="form-control" aria-describedby="e-mail" name="txtEmail" placeholder="digite aqui" required name=txtEmail />
+            </div>
+          </div>
         </div>
-        <textarea rows="20" class="form-control" aria-label="With textarea"></textarea> 
-      </div>
-    </div>
 
-    <div class="form-group ml-5 col-5">
-      <label for="txtCpf">CPF</label>
-      <input id="txtCpf" type="text" class="form-control" aria-describedby="data" name="txtCpf" placeholder="digite aqui" required name=txtCpf />
+        <div class="col mt-4">
+          <img src="https://www.carsfrombanks.com/frontend/assets/images/placeholder/inventory-full-placeholder.png" alt="..." class="img-thumbnail">
+        </div>
+      </div>
+
+      <div class="col mb-3">
+        <label for="exampleFormControlTextarea1">Descrição</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
+      </div>
+
+      <div class="col mb-3">
+        <label for="txtEmail">Banner</label>
+
+        <div class="custom-file">
+          <input type="file" class="custom-file-input" id="customFile">
+          <label class="custom-file-label" for="customFile">Escolha o arquivo</label>
+        </div>
+      </div>
+
     </div>
-    <div class="form-group ml-5 col-5 mb-5">
-      <label for="txtEmail">E-mail</label>
-      <input id="txtEmail" type="email" class="form-control" aria-describedby="e-mail" name="txtEmail" placeholder="digite aqui" required name=txtEmail />
-    </div>
+  </form>
+
 
 </section>
 
